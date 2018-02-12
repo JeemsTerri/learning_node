15fe3eb13a2eab57e2dc8102e6bbf77b9c59fd38
unknown <jeems.terry@gmail.com>
2018-02-12 18:04:46 +0700

    Adding several dependencies and initialize them in app.js

--- app.js
+++ app.js
@@ -4,6 +4,17 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local');
+var expressValidator = require('express-validator');
+var multer = require('multer');
+var upload = multer({dest: './uploads'});
+var flash = require('connect-flash');
+var bcrypt = require('bcryptjs');
+var knex = require('knex')('./knexfile');
+var bookshelf = require('bookshelf')(knex);
+
 
 var index = require('./routes/index');
 var users = require('./routes/users');
