238ae87d637861964119845619d2c4b187c67b6e
unknown <jeems.terry@gmail.com>
2018-02-12 18:35:03 +0700

    adding handle code to app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var knex = require('knex')('./knexfile');
 var bookshelf = require('bookshelf')(knex);
 
 
-var index = require('./routes/index');
+var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
@@ -30,10 +30,52 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// Handle Sessions
+app.use(session({
+  secret:'secret',
+  saveUninitialized: true,
+  resave: true
+}));
+
+// Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+app.use(flash());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+// app.get('*', function(req, res, next){
+//   res.locals.user = req.user || null;
+//   next();
+// });
+
+app.use('/', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
@@ -44,14 +86,38 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
+
+// error handler
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
 module.exports = app;
