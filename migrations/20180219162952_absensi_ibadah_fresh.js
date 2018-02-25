fef95fec65c87353db6dc56560c419a6cdef9930
unknown <jeems.terry@gmail.com>
2018-02-25 18:29:01 +0700

    fix migration file

--- migrations/20180219162952_absensi_ibadah_fresh.js
+++ migrations/20180219162952_absensi_ibadah_fresh.js
@@ -1,7 +1,7 @@
 "use strict";
 exports.up = function(knex, Promise) {
     return knex.schema.createTable('absensi_ibadah_fresh', function (t) {
-        t.increments('ibadah_fresh_id').unsigned().notNullable().references('id').inTable('ibadah_fresh')
+        t.integer('ibadah_fresh_id').unsigned().notNullable().references('id').inTable('ibadah_fresh')
         t.integer('jemaat_id').unsigned().notNullable().references('id').inTable('jemaat')
         t.string('status').notNullable()
         t.string('alasan')
