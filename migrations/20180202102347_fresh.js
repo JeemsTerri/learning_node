56af801dd256de5a6242d7b0bb881968b71ce6c0
unknown <jeems.terry@gmail.com>
2018-02-25 18:31:51 +0700

    remove unique method in fresh migration file

--- migrations/20180202102347_fresh.js
+++ migrations/20180202102347_fresh.js
@@ -2,7 +2,7 @@
 exports.up = function(knex, Promise) {
     return knex.schema.createTable('fresh', function (t) {
         t.increments('id').primary()
-        t.string('nama').notNullable().unique()
+        t.string('nama').notNullable()
         t.string('pimpinan')
         t.string('wilayah')
         t.text('description')
