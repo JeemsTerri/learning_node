bfddada6d5aa46bf41a0ba1d895139af1846d937
unknown <jeems.terry@gmail.com>
2018-02-26 11:06:24 +0700

    add primary key id

--- migrations/20180219133023_appconfig.js
+++ migrations/20180219133023_appconfig.js
@@ -1,12 +1,13 @@
 "use strict";
 exports.up = function(knex, Promise) {
     return knex.schema.createTable('appconfig', function (t) {
-        t.string('nama_gereja', 200).notNullable().primary()
+        t.increments('id').primary()
+        t.string('nama_gereja', 80).notNullable()
         t.string('alamat_gereja').notNullable()
         t.string('kota').notNullable()
         t.string('provinsi').notNullable()
         t.string('pimpinan').notNullable()
-        t.string('logo').notNullable()
+        t.string('logo').notNullable().default('noimage.svg')
         // t.timestamp('created_at').notNullable().defaultTo(knex.raw('now()'))
         // t.timestamp('updated_at').notNullable().defaultTo(knex.raw('now()'))
         // Error
